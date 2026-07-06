5027141a8b74bb32fbc3cbb2f2be2ffd173d696c
Bruno Wilson <github@janisjacobi.de>
2026-07-06 15:08:19 +0200

    begin benchmarking

---
+++ crewAiIntegrated/benchmarking/ownDocumentation.md
@@ -0,0 +1,37 @@
+# inside the physicsmodellinghelper directory:
+crewai run
+# executes the main.py file located insite the src\physicsmodellinghelper directory
+python .\src\physicsmodellinghelper\execute.py
+# use this if multiple directories are insiede src to decide which crew to run
+
+
+
+
+$trans = "..\crewRuns\session_$(Get-Date -Format yyyyMMdd_HHmmss).log"
+Start-Transcript -Path $trans
+crewai run
+Stop-Transcript
+
+# convert transcript to UTF-8 markdown
+Get-Content $trans -Raw | Set-Content ..\crewRuns\session_$(Get-Date -Format yyyyMMdd_HHmmss).md -Encoding utf8
+# for transcribing
+
+
+
+
+# venv
+# create venv
+python -m venv .venv
+
+# activate venv
+.\.venv\Scripts\Activate.ps1
+
+# install from requirements.txt
+pip install -r requirements.txt
+
+# used extension
+Markdown Preview Enhanced
+Yiyi Wang
+# allows for good md view
+# to enable markdown preview
+STRG+SHIFT+V
---
+++ crewAiIntegrated/benchmarking/sciBench/partialExecution/execute.py
@@ -0,0 +1,87 @@
+from .sourceFinding.main import run_crew_paperFinder
+from .modelling.main import run_crew_modelling
+
+def findPapers(topic : str, aim : str, outputDir : str, pdfSaveDir : str):
+    inputs = {
+        'topic': topic,
+        'aim': aim
+    }
+    result = run_crew_paperFinder(inputs = inputs, outputDir = outputDir, pdfSaveDir = pdfSaveDir)
+    return result
+
+def buildModel(topic : str, aim : str, outputDir : str, pdfSaveDir : str, previous_output : str):
+    inputs = {
+        'topic': topic,
+        'aim': aim,
+        'previous_output': previous_output
+    }
+    result = run_crew_modelling(inputs = inputs, outputDir = outputDir, pdfSaveDir = pdfSaveDir)
+    return result
+
+def findAndBuild(topic : str, aim : str, outputDir : str, pdfSaveDir : str):
+    inputs = {
+        'topic': topic,
+        'aim': aim
+    }
+    resultPapers = run_crew_paperFinder(inputs = inputs, outputDir = outputDir, pdfSaveDir = pdfSaveDir)
+    inputs2 = {
+        'topic': topic,
+        'aim': aim,
+        'previous_output': resultPapers.raw
+    }
+    outputDir = outputDir +  f"/version_1/"
+    resultModel = run_crew_modelling(inputs = inputs2, outputDir = outputDir, pdfSaveDir = pdfSaveDir)
+    return resultPapers, resultModel
+
+def execute():
+    """
+    this method runs the actual crew
+    change
+    """
+    topic = 'Edelstein-Effect'
+    aim = 'Calculate the Edelstein effect for a Rashba fermion (at the Gamma point of the Brillouin zone). ' \
+        'Compute the magnitization magnitude and direction of different directions and magnitudes of the applied electric field. '\
+        'Consider how the result depends on relevant parameters of the model (e.g. chirality, fermi velocity, spin-orbit coupling strength) and make explicit graphics.'
+    
+    outputNr = 1 # CHANGE THIS NUMBER WHEN YOU START A NEW RUN WITH NEW PAPER SEARCH
+
+    outputDir = f"./partialExecutionOutputs/runNr_{outputNr}/"
+    pdfSaveDir = f"./partialExecutionOutputs/runNr_{outputNr}/pdfs"
+
+    resultPapers = findPapers(topic = topic, aim = aim, outputDir = outputDir, pdfSaveDir = pdfSaveDir)
+    with open(f"{outputDir}/result_papers.txt", "w") as f:
+          f.write(resultPapers.raw)
+    previous_output = resultPapers.raw
+    previous_output = None
+    
+    versionNr = 9 # CHANGE THIS NUMBER WHEN YOU WANT TO KEEP THE PDF'S AND CHANGE THE MODELLING CREW
+    resultModel = buildModel(topic = topic, aim = aim, outputDir = outputDir + f"/version_{versionNr}/", pdfSaveDir = pdfSaveDir, previous_output = previous_output)
+    """
+    versionNr += 1
+    buildModel(topic = topic, aim = aim, outputDir = outputDir + f"/version_{versionNr}/", pdfSaveDir = pdfSaveDir, previous_output = previous_output)
+    """
+    return resultPapers, resultModel
+
+def executeInterface(topic : str, aim : str, outputNr : int, versionNr : int):
+    topic = 'Edelstein-Effect'
+    aim = 'Calculate the Edelstein effect for a Rashba fermion (at the Gamma point of the Brillouin zone). ' \
+        'Compute the magnitization magnitude and direction of different directions and magnitudes of the applied electric field. '\
+        'Consider how the result depends on relevant parameters of the model (e.g. chirality, fermi velocity, spin-orbit coupling strength) and make explicit graphics.'
+    
+    outputNr = 1 # CHANGE THIS NUMBER WHEN YOU START A NEW RUN WITH NEW PAPER SEARCH
+
+    outputDir = f"./sciBench/runNr_{outputNr}/"
+    pdfSaveDir = f"./sciBench/runNr_{outputNr}/pdfs"
+
+    resultPapers = findPapers(topic = topic, aim = aim, outputDir = outputDir, pdfSaveDir = pdfSaveDir)
+    with open(f"{outputDir}/result_papers.txt", "w") as f:
+          f.write(resultPapers.raw)
+    previous_output = resultPapers.raw
+    previous_output = None
+    
+    versionNr = 1 # CHANGE THIS NUMBER WHEN YOU WANT TO KEEP THE PDF'S AND CHANGE THE MODELLING CREW
+    resultModel = buildModel(topic = topic, aim = aim, outputDir = outputDir + f"/version_{versionNr}/", pdfSaveDir = pdfSaveDir, previous_output = previous_output)
+    return resultModel
+
+if __name__ == "__main__":
+    execute()
