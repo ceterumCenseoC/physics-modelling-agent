eb4d10d09335c8ebc77a57599bd815bd523f16ca
Bruno Wilson <github@janisjacobi.de>
2026-08-10 10:04:53 +0200

    format not matched

---
+++ physics_modelling_assistant/CritPt/runFailed.ps1
@@ -0,0 +1,19 @@
+$CONFIG = ".config/pma.json"
+
+$myList = @(17)
+foreach ($i in $myList) {
+
+    Write-Host "Running challenge $i"
+
+    # Read JSON
+    $json = Get-Content $CONFIG | ConvertFrom-Json
+
+    # Update challenge number
+    $json.reader_paths[0] = "data/public_test_challenges/json/Challenge_$i.json"
+
+    # Write JSON back
+    $json | ConvertTo-Json -Depth 10 | Set-Content $CONFIG
+
+    # Run command
+    python -X utf8 -m critpt generate generate model=pma/pma task_config=$CONFIG
+}
