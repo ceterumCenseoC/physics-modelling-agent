fb1c7cbddfcb64c9fe5c88b99e4c3616c3ba69dc
Bruno Wilson <github@janisjacobi.de>
2026-08-16 16:40:40 +0200

    fixing format issues of the system in run failed

--- physics_modelling_assistant/CritPt/runFailed.ps1
+++ physics_modelling_assistant/CritPt/runFailed.ps1
@@ -1,6 +1,6 @@
 $CONFIG = ".config/pma.json"
 
-$myList = @(77,8,9,10,11,12,13,14,15,16,17,18,19,120,21,22)
+$myList = @(7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,52,55,62,66,68,69,70) # needed for qwen2
 foreach ($i in $myList) {
 
     Write-Host "Running challenge $i"
@@ -19,4 +19,7 @@ foreach ($i in $myList) {
 }
 # failed DeepSeek1: 14,17,20,21,37 format issues
 #failed Qwen1: 35,37,54 format issues
-# failed Qwen2: 
+# failed Qwen2:
+# 
+
+# run DeepSeek2 from 1-6; and from 39-
